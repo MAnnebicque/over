57396e9195bace46473ba24e7eaea4860a513155
GuillaumeMartin7 <martin.guillaume19@gmail.com>
2015-06-18 18:44:24 +0200

    Ajout de la page contact & Màj bt fb

--- php/contact.php
+++ php/contact.php
@@ -1,108 +1,82 @@
+<!-- Contact form PHP script -->
+
 <?php
-/*
-* Contact Form Class
-*/
+    $name = $_POST['cd-name'];
+    $email = $_POST['cd-email'];
+    $message = $_POST['cd-textarea'];
+    $company = $_POST['cd-company'];
+    $price = $_POST['cd-price'];
+    $project = $_POST['cd-project'];
+
+    $to = "martin.guillaume19@gmail.com"; // Replace xxxx@xxxx.com with your email address (mandatory!) 
+    $subject = "Commande :". $company; // Choose a custom subject (not mandatory)
+
+    $body = $name . " (" . $email . "):\n\n" . $message ." \n \n Compagnie :  " . $company ." \n Budget : ". $price ."\n \n Project : ". $project . ;
+
+    $from = "wwww.guillaumemartin.pe.hu"; // Replace "Beetle Template" with your site name (not mandatory)   
+    $headers = "From:" . $from . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();    
 
 
-header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Content-type: application/json');
-
-$admin_email = 'your@yourdomain.com'; // Your Email
-$message_min_length = 5; // Min Message Length
 
 
-class Contact_Form{
-	function __construct($details, $email_admin, $message_min_length){
-		
-		$this->name = stripslashes($details['name']);
-		$this->email = trim($details['email']);
-		$this->subject = 'Contact from Your Website'; // Subject 
-		$this->message = stripslashes($details['message']);
-	
-		$this->email_admin = $email_admin;
-		$this->message_min_length = $message_min_length;
-		
-		$this->response_status = 1;
-		$this->response_html = '';
-	}
 
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ($name != '' && $email != '' && $message != '') {
+                
+            if (mail ($to, $subject, $body, $headers)) { 
+                echo '<p style="color:#66A325;">Thanks! Your message has been sent.</p>';
+            } else { 
+                echo '<p style="color:#F84B3C;">Something went wrong, go back and try again!</p>'; 
+            } 
 
-	private function validateEmail(){
-		$regex = '/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i';
-	
-		if($this->email == '') { 
-			return false;
-		} else {
-			$string = preg_replace($regex, '', $this->email);
-		}
-	
-		return empty($string) ? true : false;
-	}
+        } else {
+            echo '<p style="color:#F84B3C;">You need to fill in all required fields!</p>';
+        }
+    } else {
+        echo '<p style="color:#F84B3C;">Invalid Email, please provide an correct email.</p>';
+    }  
 
+    // Use the script below if you want to use HTML emails instead
 
-	private function validateFields(){
-		// Check name
-		if(!$this->name)
-		{
-			$this->response_html .= '<p>Please enter your name</p>';
-			$this->response_status = 0;
-		}
+    /*
 
-		// Check email
-		if(!$this->email)
-		{
-			$this->response_html .= '<p>Please enter an e-mail address</p>';
-			$this->response_status = 0;
-		}
-		
-		// Check valid email
-		if($this->email && !$this->validateEmail())
-		{
-			$this->response_html .= '<p>Please enter a valid e-mail address</p>';
-			$this->response_status = 0;
-		}
-		
-		// Check message length
-		if(!$this->message || strlen($this->message) < $this->message_min_length)
-		{
-			$this->response_html .= '<p>Please enter your message. It should have at least '.$this->message_min_length.' characters</p>';
-			$this->response_status = 0;
-		}
-	}
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
+    $to = 'xxxx@xxxx.com'; // Replace xxxx@xxxx.com with your email address (mandatory!) 
+    $subject = 'Hello'; // Choose a custom subject (not mandatory)
 
-	private function sendEmail(){
-		$mail = mail($this->email_admin, $this->subject, $this->message,
-			 "From: ".$this->name." <".$this->email.">\r\n"
-			."Reply-To: ".$this->email."\r\n"
-		."X-Mailer: PHP/" . phpversion());
-	
-		if($mail)
-		{
-			$this->response_status = 1;
-			$this->response_html = '<p>Thank You!</p>';
-		}
-	}
+    $body = '<html><body>';
+    $body .= '<em style="color: #999;">You have received a message from <strong>' . $name . '</strong> (' . $email . '):</em>';
+    $body .= '<p>' . $message . '</p>';
+    $body .= '</body></html>';
 
+    $from = "From: Beetle Template"; // Replace "Beetle Template" with your site name (not mandatory)   
+    $headers = "From:" . $from . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();    
+                
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ($name != '' && $email != '' && $message != '') {
+                
+            if (mail ($to, $subject, $body, $headers)) { 
+                echo '<p style="color:#66A325;">Thanks! Your message has been sent.</p>';
+            } else { 
+                echo '<p style="color:#F84B3C;">Something went wrong, go back and try again!</p>'; 
+            } 
 
-	function sendRequest(){
-		$this->validateFields();
-		if($this->response_status)
-		{
-			$this->sendEmail();
-		}
+        } else {
+            echo '<p style="color:#F84B3C;">You need to fill in all required fields!</p>';
+        }
+    } else {
+        echo '<p style="color:#F84B3C;">Invalid Email, please provide an correct email.</p>';
+    }   
 
-		$response = array();
-		$response['status'] = $this->response_status;	
-		$response['html'] = $this->response_html;
-		
-		echo json_encode($response);
-	}
-}
-
-
-$contact_form = new Contact_Form($_POST, $admin_email, $message_min_length);
-$contact_form->sendRequest();
+    */  
 
 ?>
